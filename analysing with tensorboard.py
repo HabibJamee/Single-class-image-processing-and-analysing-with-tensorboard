228c52a6102941f09d3ec74aef94714e61ea164e
habib_jamee <43148756+HabibJamee@users.noreply.github.com>
2020-08-23 15:16:16 +0600

    Update analysing with tensorboard.py

--- analysing with tensorboard.py
+++ analysing with tensorboard.py
@@ -8,4 +8,13 @@ model.fit(X, y,
           validation_split=0.3,
           callbacks=[tensorboard])
           
-
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+# more info on callbakcs: https://keras.io/callbacks/ model saver is cool too.
+from tensorflow.keras.callbacks import TensorBoard
+from numpy import array
+import pickle
+import time
