6a3eb37457e6a49ed77fc7b3a8978e6b91c4bc23
habib_jamee <43148756+HabibJamee@users.noreply.github.com>
2020-09-06 10:02:59 +0600

    Update analysing with tensorboard.py

--- analysing with tensorboard.py
+++ analysing with tensorboard.py
@@ -18,3 +18,47 @@ from tensorflow.keras.callbacks import TensorBoard
 from numpy import array
 import pickle
 import time
+
+
+NAME = ""
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = array(pickle.load(open("y.pickle","rb")))
+
+X = X/255.0
+
+model = Sequential()
+
+model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'],
+              )
+
+model.fit(X, y,
+          batch_size=10,
+          epochs=50,
+          validation_split=0.3,
+          callbacks=[tensorboard])
